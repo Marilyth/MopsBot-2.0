0f3485ca14d556093f8424b9ccc2a9b9c11d9813
May <baermay98@gmail.com>
2018-01-02 21:48:45 +0100

    Crashsupport for Chart

--- Module/Data/Session/TwitchTracker.cs
+++ Module/Data/Session/TwitchTracker.cs
@@ -37,7 +37,9 @@ namespace MopsBot.Module.Data.Session
             isOnline = pIsOnline;
             curGame = pGame;
 
-            gameChange();
+            if (isOnline) readPlotPoints();
+
+            else gameChange();
 
             checkForChange = new System.Threading.Timer(CheckForChange_Elapsed, new System.Threading.AutoResetEvent(false), StaticBase.ran.Next(6, 59) * 1000, 60000);
         }
@@ -62,6 +64,9 @@ namespace MopsBot.Module.Data.Session
                 {
                     isOnline = false;
                     Console.Out.WriteLine($"{DateTime.Now} {name} went Offline");
+                    var file = new FileInfo($"data//plots//{name}.txt");
+                    file.Delete();
+                    initViewerChart();
                 }
                 else
                 {
@@ -173,6 +178,8 @@ namespace MopsBot.Module.Data.Session
             var files = dir.GetFiles().Where(x => x.Extension.ToLower().Equals($"{name}.pdf"));
             foreach (var f in files)
                 f.Delete();
+
+            writePlotPoints();
         }
 
         private void initViewerChart()
@@ -180,13 +187,31 @@ namespace MopsBot.Module.Data.Session
             viewerChart = new PlotModel();
             viewerChart.TextColor = OxyColor.FromRgb(175, 175, 175);
             viewerChart.PlotAreaBorderThickness = new OxyThickness(0);
-            var valueAxisY = new OxyPlot.Axes.LinearAxis { Position = OxyPlot.Axes.AxisPosition.Left, TicklineColor = OxyColor.FromRgb(125, 125, 155), Title = "Viewers", Minimum = 0, FontSize = 13
-            , AxislineStyle = LineStyle.Solid, AxislineColor = OxyColor.FromRgb(125, 125, 155)};
-            var valueAxisX = new OxyPlot.Axes.LinearAxis { Position = OxyPlot.Axes.AxisPosition.Bottom, TicklineColor = OxyColor.FromRgb(125, 125, 155), Title = "Time in Minutes", FontSize = 13
-            , AxislineStyle = LineStyle.Solid, AxislineColor = OxyColor.FromRgb(125, 125, 155)};
+            var valueAxisY = new OxyPlot.Axes.LinearAxis
+            {
+                Position = OxyPlot.Axes.AxisPosition.Left,
+                TicklineColor = OxyColor.FromRgb(125, 125, 155),
+                Title = "Viewers",
+                Minimum = 0,
+                FontSize = 13,
+                AxislineStyle = LineStyle.Solid,
+                AxislineColor = OxyColor.FromRgb(125, 125, 155)
+            };
+
+            var valueAxisX = new OxyPlot.Axes.LinearAxis
+            {
+                Position = OxyPlot.Axes.AxisPosition.Bottom,
+                TicklineColor = OxyColor.FromRgb(125, 125, 155),
+                Title = "Time in Minutes",
+                FontSize = 13,
+                AxislineStyle = LineStyle.Solid,
+                AxislineColor = OxyColor.FromRgb(125, 125, 155)
+            };
+
             viewerChart.Axes.Add(valueAxisY);
             viewerChart.Axes.Add(valueAxisX);
             viewerChart.LegendFontSize = 13;
+            viewerChart.LegendPosition = LegendPosition.BottomCenter;
 
             series = new Dictionary<string, OxyPlot.Series.LineSeries>();
         }
@@ -197,6 +222,61 @@ namespace MopsBot.Module.Data.Session
             series[curGame].Color = OxyColor.FromRgb((byte)StaticBase.ran.Next(30, 220), (byte)StaticBase.ran.Next(30, 220), (byte)StaticBase.ran.Next(30, 220));
             series[curGame].Title = curGame;
             viewerChart.Series.Add(series[curGame]);
+        }
+
+        private void gameChange(string newGame)
+        {
+            if (!series.ContainsKey(newGame))
+            {
+                series.Add(newGame, new OxyPlot.Series.LineSeries());
+                series[newGame].Color = OxyColor.FromRgb((byte)StaticBase.ran.Next(30, 220), (byte)StaticBase.ran.Next(30, 220), (byte)StaticBase.ran.Next(30, 220));
+                series[newGame].Title = newGame;
+                viewerChart.Series.Add(series[newGame]);
+            }
+        }
+
+        public void recolour()
+        {
+            initViewerChart();
+            series = new Dictionary<string, OxyPlot.Series.LineSeries>();
+            readPlotPoints();
+        }
+
+        private void writePlotPoints()
+        {
+            StreamWriter write = new StreamWriter(new FileStream($"data//plots//{name}.txt", FileMode.Create));
+            write.AutoFlush = true;
+            write.WriteLine(columnCount);
+            foreach (var game in series.Keys)
+            {
+                write.WriteLine($"GAMECHANGE:{game}");
+                foreach (var point in series[game].Points)
+                {
+                    write.WriteLine($"{point.X}:{point.Y}");
+                }
+            }
+            write.Dispose();
+        }
+
+        private void readPlotPoints()
+        {
+            StreamReader read = new StreamReader(new FileStream($"data//plots//{name}.txt", FileMode.OpenOrCreate));
+
+            columnCount = int.Parse(read.ReadLine());
+            string currentGame = "";
+            string s = "";
+            while ((s = read.ReadLine()) != null)
+            {
+                if (s.StartsWith("GAMECHANGE"))
+                {
+                    currentGame = s.Split(":")[1];
+                    gameChange(currentGame);
+                }
+                else
+                    series[currentGame].Points.Add(new DataPoint(double.Parse(s.Split(":")[0]), double.Parse(s.Split(":")[1])));
+            }
+
+            read.Dispose();
         }
     }
 }
