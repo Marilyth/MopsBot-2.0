7de9bcfdf7c68e2e064ad695692aac481900d605
Marilyth <baermay98@gmail.com>
2018-04-11 15:38:57 +0200

    Extract Twitch plot to own class.

--- Data/Session/TwitchTracker.cs
+++ Data/Session/TwitchTracker.cs
@@ -6,7 +6,6 @@ using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using MopsBot.Data.Session.APIResults;
-using OxyPlot;
 using System.IO;
 using System.Runtime.InteropServices;
 using Microsoft.Win32.SafeHandles;
@@ -15,9 +14,7 @@ namespace MopsBot.Data.Session
 {
     public class TwitchTracker : ITracker
     {
-        private PlotModel viewerChart;
-        private Dictionary<string, List<OxyPlot.Series.LineSeries>> series;
-        private int columnCount;
+        private Plot viewerGraph;
         public Dictionary<ulong, Discord.IUserMessage> ToUpdate;
         public Boolean IsOnline;
         public string Name, CurGame;
@@ -26,7 +23,7 @@ namespace MopsBot.Data.Session
 
         public TwitchTracker(string streamerName) : base(60000)
         {
-            initViewerChart();
+            viewerGraph = new Plot(streamerName, "Time In Minutes","Viewers", false);
 
             Console.Out.WriteLine($"{DateTime.Now} Started Twitchtracker for {streamerName}");
             ToUpdate = new Dictionary<ulong, Discord.IUserMessage>();
@@ -37,12 +34,13 @@ namespace MopsBot.Data.Session
 
         public TwitchTracker(string[] initArray) : base(60000)
         {
-            initViewerChart();
             ToUpdate = new Dictionary<ulong, Discord.IUserMessage>();
             ChannelMessages = new Dictionary<ulong, string>();
 
             Name = initArray[0];
             IsOnline = Boolean.Parse(initArray[1]);
+            viewerGraph = new Plot(Name, "Time In Minutes","Viewers", IsOnline);
+
             foreach (string channel in initArray[2].Split(new char[] { '{', '}', ';' }))
             {
                 if (channel != "")
@@ -66,7 +64,6 @@ namespace MopsBot.Data.Session
                     }
                 }
                 CurGame = streamerInformation().stream.game;
-                readPlotPoints();
             }
         }
 
@@ -88,11 +85,9 @@ namespace MopsBot.Data.Session
                 if (IsOnline)
                 {
                     IsOnline = false;
-                    columnCount = 0;
                     Console.Out.WriteLine($"{DateTime.Now} {Name} went Offline");
-                    var file = new FileInfo($"mopsdata//plots//{Name}.txt");
-                    file.Delete();
-                    initViewerChart();
+                    viewerGraph.RemovePlot();
+                    viewerGraph = new Plot(Name, "Time In Minutes", "Viewers", false);
 
                     foreach (ulong channel in ChannelMessages.Keys)
                         await OnMinorChangeTracked(channel, $"{Name} went Offline!");
@@ -103,7 +98,7 @@ namespace MopsBot.Data.Session
                     IsOnline = true;
                     ToUpdate = new Dictionary<ulong, Discord.IUserMessage>();
                     CurGame = StreamerStatus.stream.game;
-                    gameChange();
+                    viewerGraph.SwitchTitle(CurGame);
 
                     foreach (ulong channel in ChannelMessages.Keys)
                         await OnMinorChangeTracked(channel, ChannelMessages[channel]);
@@ -112,23 +107,17 @@ namespace MopsBot.Data.Session
 
             if (IsOnline)
             {
-                columnCount++;
-
-                if (!series.ContainsKey(CurGame))
-                    gameChange(CurGame);
-                series[CurGame].Last().Points.Add(new DataPoint(columnCount, StreamerStatus.stream.viewers));
+                viewerGraph.AddValue(StreamerStatus.stream.viewers);
 
                 if (CurGame.CompareTo(StreamerStatus.stream.game) != 0)
                 {
                     CurGame = StreamerStatus.stream.game;
-                    gameChange();
-
-                    series[CurGame].Last().Points.Add(new DataPoint(columnCount, StreamerStatus.stream.viewers));
+                    viewerGraph.SwitchTitle(CurGame);
+                    viewerGraph.AddValue(StreamerStatus.stream.viewers);
 
                     foreach (ulong channel in ChannelMessages.Keys)
                         await OnMinorChangeTracked(channel, $"{Name} switched games to **{CurGame}**");
                 }
-                updateChart();
                 foreach (ulong channel in ChannelIds)
                     await OnMajorChangeTracked(channel, createEmbed());
             }
@@ -171,143 +160,12 @@ namespace MopsBot.Data.Session
             e.Footer = footer;
 
             e.ThumbnailUrl = $"{StreamerStatus.stream.preview.medium}?rand={StaticBase.ran.Next(0, 99999999)}";
-            e.ImageUrl = $"http://5.45.104.29/StreamCharts/{Name}plot.png?rand={StaticBase.ran.Next(0, 99999999)}";
+            e.ImageUrl = $"{viewerGraph.DrawPlot()}";
 
             e.AddInlineField("Game", CurGame);
             e.AddInlineField("Viewers", StreamerStatus.stream.viewers);
 
             return e;
-        }
-
-
-        private void updateChart()
-        {
-            using (var stream = File.Create($"mopsdata//{Name}plot.pdf"))
-            {
-                var pdfExporter = new PdfExporter { Width = 800, Height = 400 };
-                pdfExporter.Export(viewerChart, stream);
-            }
-
-            var prc = new System.Diagnostics.Process();
-            prc.StartInfo.FileName = "convert";
-            prc.StartInfo.Arguments = $"-set density 300 \"mopsdata//{Name}plot.pdf\" \"//var//www//html//StreamCharts//{Name}plot.png\"";
-
-            prc.Start();
-
-            prc.WaitForExit();
-
-            var dir = new DirectoryInfo("mopsdata//");
-            var files = dir.GetFiles().Where(x => x.Extension.ToLower().Equals($"{Name}.pdf"));
-            foreach (var f in files)
-                f.Delete();
-
-            writePlotPoints();
-        }
-
-        private void initViewerChart()
-        {
-            viewerChart = new PlotModel();
-            viewerChart.TextColor = OxyColor.FromRgb(175, 175, 175);
-            viewerChart.PlotAreaBorderThickness = new OxyThickness(0);
-            var valueAxisY = new OxyPlot.Axes.LinearAxis
-            {
-                Position = OxyPlot.Axes.AxisPosition.Left,
-                TicklineColor = OxyColor.FromRgb(125, 125, 155),
-                Title = "Viewers",
-                Minimum = 0,
-                FontSize = 24,
-                AxislineStyle = LineStyle.Solid,
-                AxislineColor = OxyColor.FromRgb(125, 125, 155)
-            };
-
-            var valueAxisX = new OxyPlot.Axes.LinearAxis
-            {
-                Position = OxyPlot.Axes.AxisPosition.Bottom,
-                TicklineColor = OxyColor.FromRgb(125, 125, 155),
-                Title = "Time in Minutes",
-                FontSize = 24,
-                AxislineStyle = LineStyle.Solid,
-                AxislineColor = OxyColor.FromRgb(125, 125, 155)
-            };
-
-            viewerChart.Axes.Add(valueAxisY);
-            viewerChart.Axes.Add(valueAxisX);
-            viewerChart.LegendFontSize = 24;
-            viewerChart.LegendPosition = LegendPosition.BottomCenter;
-
-            series = new Dictionary<string, List<OxyPlot.Series.LineSeries>>();
-        }
-
-        private void gameChange()
-        {
-            gameChange(CurGame);
-        }
-
-        private void gameChange(string newGame)
-        {
-            if (!series.ContainsKey(newGame))
-            {
-                series.Add(newGame, new List<OxyPlot.Series.LineSeries>());
-                series[newGame].Add(new OxyPlot.Series.LineSeries());
-                series[newGame].Last().Color = OxyColor.FromRgb((byte)StaticBase.ran.Next(30, 220), (byte)StaticBase.ran.Next(30, 220), (byte)StaticBase.ran.Next(30, 220));
-                series[newGame].Last().Title = newGame;
-                series[newGame].Last().StrokeThickness = 3;
-                viewerChart.Series.Add(series[newGame].Last());
-            }
-            else
-            {
-                series[newGame].Add(new OxyPlot.Series.LineSeries());
-                series[newGame].Last().Color = series[newGame].First().Color;
-                series[newGame].Last().StrokeThickness = 3;
-                viewerChart.Series.Add(series[newGame].Last());
-            }
-        }
-
-        public void recolour()
-        {
-            initViewerChart();
-            series = new Dictionary<string, List<OxyPlot.Series.LineSeries>>();
-            readPlotPoints();
-        }
-
-        private void writePlotPoints()
-        {
-            using (StreamWriter write = new StreamWriter(new FileStream($"mopsdata//plots//{Name}.txt", FileMode.Create)))
-            {
-                write.WriteLine(columnCount);
-                foreach (var game in series.Keys)
-                {
-                    foreach (var lineseries in series[game])
-                    {
-                        write.WriteLine($"GAMECHANGE={game}");
-                        foreach (var point in lineseries.Points)
-                        {
-                            write.WriteLine($"{point.X}:{point.Y}");
-                        }
-                    }
-                }
-            }
-        }
-
-        private void readPlotPoints()
-        {
-
-            using (StreamReader read = new StreamReader(new FileStream($"mopsdata//plots//{Name}.txt", FileMode.OpenOrCreate)))
-            {
-                columnCount = int.Parse(read.ReadLine());
-                string currentGame = "";
-                string s = "";
-                while ((s = read.ReadLine()) != null)
-                {
-                    if (s.StartsWith("GAMECHANGE"))
-                    {
-                        currentGame = s.Split("=")[1];
-                        gameChange(currentGame);
-                    }
-                    else
-                        series[currentGame].Last().Points.Add(new DataPoint(double.Parse(s.Split(":")[0]), double.Parse(s.Split(":")[1])));
-                }
-            }
         }
 
         public override string[] GetInitArray()
