75589f8e8896eb71842d8a86202cb1dc89d4cdda
Marilyth <baermay98@gmail.com>
2018-06-08 16:05:45 +0200

    Switch to myget beta

--- CommandHandler.cs
+++ CommandHandler.cs
@@ -33,7 +33,7 @@ namespace MopsBot
             commands = new CommandService();
             //_map.Add(commands);
 
-            await commands.AddModulesAsync(Assembly.GetEntryAssembly());
+            await commands.AddModulesAsync(Assembly.GetEntryAssembly(), provider);
 
             guildPrefix = new Dictionary<ulong, string>();
             fillPrefix();
