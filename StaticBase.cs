048a60539ea10bc4cf450d10b8fe17ca4077473e
Marilyth <baermay98@gmail.com>
2018-09-04 15:12:29 +0200

    Database url hidden

--- StaticBase.cs
+++ StaticBase.cs
@@ -40,7 +40,7 @@ namespace MopsBot
         public static Dictionary<string, TrackerWrapper> Trackers;
         public static MuteTimeHandler MuteHandler;
         public static NewsApiClient NewsClient;
-        public static MongoClient DatabaseClient = new MongoClient($"mongodb://Mayfunction:{Program.Config["MopsAPI"]}@5.45.104.29:27017");
+        public static MongoClient DatabaseClient = new MongoClient($"{Program.Config["DatabaseURL"]}");
         public static IMongoDatabase Database = DatabaseClient.GetDatabase("Mops");
 
         public static bool init = false;
