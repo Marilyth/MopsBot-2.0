a54ce137856fbd0c029f88f4d4613bf4bb559d2e
Marilyth <baermay98@gmail.com>
2018-09-05 18:55:35 +0200

    Rename

--- Module/Game.cs
+++ Module/Game.cs
@@ -12,12 +12,12 @@ namespace MopsBot.Module
 {
     public class Game : ModuleBase
     {
-        [Group("Fight")]
+        [Group("RPG")]
         [RequireBotPermission(ChannelPermission.SendMessages)]
         [RequireBotPermission(ChannelPermission.ManageMessages)]
-        public class FightGame : ModuleBase
+        public class RPG : ModuleBase
         {
-            [Command("Start")]
+            [Command("Fight")]
             [Summary("Starts a fight with the specified Enemy.")]
             public async Task Start([Remainder]string enemy){
                 var message = await ReplyAsync("Generating fight.");
@@ -31,17 +31,6 @@ namespace MopsBot.Module
                     await ReplyAsync(string.Join(", ", StaticBase.Database.GetCollection<Data.Entities.Enemy>("Enemies").FindSync(x => true).ToList().Select(y => y.Name)));
                 else
                     await ReplyAsync("", embed: StaticBase.Database.GetCollection<Data.Entities.Enemy>("Enemies").FindSync(x => x.Name.Equals(enemy)).First().StatEmbed());
-            }
-
-            [Command("Write")]
-            [Summary("Lists all available enemies, or specific information on one enemy.")]
-            public async Task write(){
-                Data.Entities.ItemMove moveTest = new Data.Entities.ItemMove(){Name = "Slash", DamageModifier = 1, RageConsumption = 0, DefenceModifier = 1};
-                Data.Entities.Item test = new Data.Entities.Item(){Id = 1, Name = "Sword", BaseDamage = 2, BaseDefence = 1, Moveset = new List<Data.Entities.ItemMove>(){moveTest}};
-                Data.Entities.Enemy enemyTest = new Data.Entities.Enemy(){Name = "Skeleton", Damage = 2, Defence = 1, Health = 20, MoveList = new List<Data.Entities.ItemMove>(){moveTest}, LootChance = new Dictionary<int, int>(){{1, 50}}};
-
-                StaticBase.Database.GetCollection<Data.Entities.Item>("Items").InsertOne(test);
-                StaticBase.Database.GetCollection<Data.Entities.Enemy>("Enemies").InsertOne(enemyTest);
             }
         }
 
