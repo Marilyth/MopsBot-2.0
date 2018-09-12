1e28d19c9113275fe6beb7753e86faee19ef65a4
Marilyth <baermay98@gmail.com>
2018-09-12 21:12:56 +0200

    Mongodb is a diva

--- Data/Tracker/TwitchClipTracker.cs
+++ Data/Tracker/TwitchClipTracker.cs
@@ -9,12 +9,16 @@ using Newtonsoft.Json;
 using MopsBot.Data.Tracker.APIResults.TwitchClip;
 using System.Threading.Tasks;
 using System.Xml;
+using MongoDB.Bson.Serialization.Options;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace MopsBot.Data.Tracker
 {
     public class TwitchClipTracker : ITracker
     {
         public uint ViewThreshold;
+        
+        [BsonDictionaryOptions(DictionaryRepresentation.ArrayOfDocuments)]
         public Dictionary<DateTime, KeyValuePair<int, double>> TrackedClips;
         public TwitchClipTracker() : base(600000, (ExistingTrackers * 2000 + 500) % 600000)
         {
