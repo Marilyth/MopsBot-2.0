b639d4164abd5e0c835238ac57b51b640f04e638
Marilyth <baermay98@gmail.com>
2018-09-18 15:10:04 +0200

    Stop checking for files

--- Program.cs
+++ Program.cs
@@ -28,17 +28,9 @@ namespace MopsBot
         public static Dictionary<string, string> Config;
         public static CommandHandler Handler { get; private set; }
         public static ReactionHandler ReactionHandler { get; private set; }
-        private System.Threading.Timer openFileChecker;
-
-        //Open file handling
-        private int ProcessId, OpenFilesCount, OpenFilesRepetition;
-        private static int OpenFilesRepetitionThreshold = 5;
 
         private async Task Start()
         {
-            ProcessId = System.Diagnostics.Process.GetCurrentProcess().Id;
-            Console.Out.WriteLine(ProcessId);
-
             Client = new DiscordSocketClient(new DiscordSocketConfig()
             {
                 LogLevel = LogSeverity.Info,
@@ -66,8 +58,6 @@ namespace MopsBot
             ReactionHandler = new ReactionHandler();
             ReactionHandler.Install(provider);
 
-            openFileChecker = new System.Threading.Timer(checkOpenFiles, null, 60000, 60000);
-
             await Task.Delay(-1);
         }
 
@@ -84,54 +74,6 @@ namespace MopsBot
             Task.Run(() => StaticBase.UpdateGameAsync());
             return Task.CompletedTask;
         }
-
-        private void checkOpenFiles(object stateinfo)
-        {
-            try
-            {
-                using (var process = new System.Diagnostics.Process())
-                {
-                    process.StartInfo.FileName = "/bin/bash";
-                    process.StartInfo.Arguments = $"-c \"ls -lisa /proc/{ProcessId}/fd | wc -l\"";
-                    process.StartInfo.RedirectStandardOutput = true;
-                    process.StartInfo.UseShellExecute = false;
-                    process.StartInfo.CreateNoWindow = true;
-
-                    process.Start();
-                    process.WaitForExit();
-
-                    string result = process.StandardOutput.ReadToEnd();
-                    int openFiles = Convert.ToInt32(result);
-                    Console.WriteLine("\n" + System.DateTime.Now + $" open files were {openFiles}");
-                   
-                    if (OpenFilesCount == openFiles){
-                        if(++OpenFilesRepetition == OpenFilesRepetitionThreshold){
-                            Console.WriteLine("\nShutting down due to 5 repetitions!");
-                            Environment.Exit(-1);
-                        }
-                    }
-
-                    else
-                        OpenFilesRepetition = 0;
-                    
-                    
-                    if (OpenFilesCount > 600){
-                        Console.WriteLine("\nShutting down due to too many open files!");
-                        Environment.Exit(-1);
-                    }
-
-                    OpenFilesCount = openFiles;
-                }
-
-
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("\n[FILE READING ERROR]: " + System.DateTime.Now + $" {e.Message}\n{e.StackTrace}");
-                //Environment.Exit(-1);
-            }
-        }
-
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseUrls("http://0.0.0.0:5000/")
