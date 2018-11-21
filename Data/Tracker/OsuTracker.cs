53cea041db2c5615e4c65fd0f0e047c3165f6a6f
Marilyth <baermay98@gmail.com>
2018-11-21 14:02:44 +0100

    Add mod descriptions to osu tracking

--- Data/Tracker/OsuTracker.cs
+++ Data/Tracker/OsuTracker.cs
@@ -57,7 +57,8 @@ namespace MopsBot.Data.Tracker
                 {
                     OsuResult userInformation = await fetchUser(pp.Key);
                     if (userInformation == null) return;
-                    if (pp.Value == 0){
+                    if (pp.Value == 0)
+                    {
                         allPP[pp.Key] = allPP[pp.Key] = double.Parse(userInformation.pp_raw ?? "0", CultureInfo.InvariantCulture);
                         continue;
                     }
@@ -66,11 +67,12 @@ namespace MopsBot.Data.Tracker
                     {
                         var recentScores = await fetchRecent(pp.Key);
 
-                        if(recentScores == null){
+                        if (recentScores == null)
+                        {
                             allPP[pp.Key] = double.Parse(userInformation.pp_raw ?? "0", CultureInfo.InvariantCulture);
                             return;
                         }
-                        
+
                         RecentScore scoreInformation = recentScores.First(x => !x.rank.Equals("F"));
 
                         Beatmap beatmapInformation = await fetchBeatmap(scoreInformation.beatmap_id, pp.Key);
@@ -86,8 +88,8 @@ namespace MopsBot.Data.Tracker
             }
             catch (Exception e)
             {
-                if(!e.StackTrace.StartsWith("The read operation failed"))
-                    Console.WriteLine("\n" +  $"[ERROR] by {Name} at {DateTime.Now}:\n{e.Message}\n{e.StackTrace}");
+                if (!e.StackTrace.StartsWith("The read operation failed"))
+                    Console.WriteLine("\n" + $"[ERROR] by {Name} at {DateTime.Now}:\n{e.Message}\n{e.StackTrace}");
             }
         }
 
@@ -126,7 +128,7 @@ namespace MopsBot.Data.Tracker
             e.Color = new Color(255, 87, 156);
             e.Title = $"{beatmapInformation.artist} - {beatmapInformation.title} [{beatmapInformation.version}]";
             e.Url = $"https://osu.ppy.sh/b/{beatmapInformation.beatmap_id}&m={beatmapInformation.mode}";
-            e.Description = Math.Round(double.Parse(beatmapInformation.difficultyrating, CultureInfo.InvariantCulture), 2) + "*";
+            e.Description = Math.Round(double.Parse(beatmapInformation.difficultyrating, CultureInfo.InvariantCulture), 2) + "*, " + (Mods)int.Parse(scoreInformation.enabled_mods);
             e.Timestamp = DateTime.Parse(scoreInformation.date).AddHours(-6);
 
             EmbedAuthorBuilder author = new EmbedAuthorBuilder();
@@ -201,7 +203,48 @@ namespace MopsBot.Data.Tracker
             return 0;
         }
 
-        public override string TrackerUrl(){
+        [Flags]
+        public enum Mods
+        {
+            NoMod = 0,
+            NoFail = 1,
+            Easy = 2,
+            TouchDevice = 4,
+            Hidden = 8,
+            HardRock = 16,
+            SuddenDeath = 32,
+            DoubleTime = 64,
+            Relax = 128,
+            HalfTime = 256,
+            Nightcore = 512, // Only set along with DoubleTime. i.e: NC only gives 576
+            Flashlight = 1024,
+            Autoplay = 2048,
+            SpunOut = 4096,
+            Relax2 = 8192,  // Autopilot
+            Perfect = 16384, // Only set along with SuddenDeath. i.e: PF only gives 16416  
+            Key4 = 32768,
+            Key5 = 65536,
+            Key6 = 131072,
+            Key7 = 262144,
+            Key8 = 524288,
+            FadeIn = 1048576,
+            Random = 2097152,
+            Cinema = 4194304,
+            Target = 8388608,
+            Key9 = 16777216,
+            KeyCoop = 33554432,
+            Key1 = 67108864,
+            Key3 = 134217728,
+            Key2 = 268435456,
+            ScoreV2 = 536870912,
+            LastMod = 1073741824,
+            KeyMod = Key1 | Key2 | Key3 | Key4 | Key5 | Key6 | Key7 | Key8 | Key9 | KeyCoop,
+            FreeModAllowed = NoFail | Easy | Hidden | HardRock | SuddenDeath | Flashlight | FadeIn | Relax | Relax2 | SpunOut | KeyMod,
+            ScoreIncreaseMods = Hidden | HardRock | DoubleTime | Flashlight | FadeIn
+        }
+
+        public override string TrackerUrl()
+        {
             return "https://osu.ppy.sh/u/" + Name;
         }
     }
