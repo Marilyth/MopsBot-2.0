55bb75e8092ee26c6b350acdf8100445e6cdee86
Marilyth <baermay98@gmail.com>
2018-12-31 14:26:34 +0100

    Convert totalminutes to int

--- Data/Tracker/TwitchTracker.cs
+++ Data/Tracker/TwitchTracker.cs
@@ -244,7 +244,7 @@ namespace MopsBot.Data.Tracker
                     TimeSpan duration = i != games.Count - 1 ? OxyPlot.Axes.DateTimeAxis.ToDateTime(games[i + 1].Value) - OxyPlot.Axes.DateTimeAxis.ToDateTime(games[i].Value)
                                                              : DateTime.UtcNow - OxyPlot.Axes.DateTimeAxis.ToDateTime(games[i].Value);
                     TimeSpan timestamp = OxyPlot.Axes.DateTimeAxis.ToDateTime(games[i].Value) - OxyPlot.Axes.DateTimeAxis.ToDateTime(games[0].Value);
-                    e.Description += $"\n[{games[i].Key}]({VodUrl}?t={timestamp.TotalMinutes}m) ({duration.Hours}h {duration.Minutes}m)";
+                    e.Description += $"\n[{games[i].Key}]({VodUrl}?t={(int)timestamp.TotalMinutes}m) ({duration.Hours}h {duration.Minutes}m)";
                 }
             }
 
