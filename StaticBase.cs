2101b9e5d918221baacaa2fec303dfbd73f44ac1
May Baer <baermay98@gmail.com>
2019-01-25 13:35:52 +0100

    Set custom user agent

--- StaticBase.cs
+++ StaticBase.cs
@@ -53,6 +53,7 @@ namespace MopsBot
         {
             if (!init)
             {
+                HttpClient.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)");
                 ServicePointManager.ServerCertificateValidationCallback = (object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) => {return true;};
                 ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls|SecurityProtocolType.Tls11|SecurityProtocolType.Tls12;
                 HttpClient.DefaultRequestHeaders.ConnectionClose = true;
