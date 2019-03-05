0a53951fee1b89fa42dfa529396b7fc1b08a4750
May Baer <baermay98@gmail.com>
2019-03-05 15:40:53 +0100

    Add graph and always keywords to JSONTracker

--- Data/Tracker/JSONTracker.cs
+++ Data/Tracker/JSONTracker.cs
@@ -22,6 +22,7 @@ namespace MopsBot.Data.Tracker
         [MongoDB.Bson.Serialization.Attributes.BsonDictionaryOptions(MongoDB.Bson.Serialization.Options.DictionaryRepresentation.ArrayOfDocuments)]
         public Dictionary<string, string> PastInformation;
         public List<string> ToTrack;
+        public DatePlot DataGraph;
 
         public JSONTracker() : base(300000, ExistingTrackers * 2000)
         {
@@ -67,12 +68,29 @@ namespace MopsBot.Data.Tracker
             try
             {
                 PastInformation = getResults().Result;
+                var graphTest = PastInformation.FirstOrDefault(x => x.Key.StartsWith("graph:"));
+                if(!graphTest.Equals(default(KeyValuePair<string,string>))){
+                    bool succeeded = double.TryParse(graphTest.Value, out double test);
+
+                    if(succeeded){
+                        DataGraph = new DatePlot("JSON" + Name.GetHashCode(), "Date", "Value", format: "dd-MMM", relativeTime: false);
+                        DataGraph.AddValue("Value", test, relative:false);
+                    }
+
+                    else throw new Exception("Graph value is not a number!");
+                }
             }
             catch (Exception)
             {
                 Dispose();
                 throw new Exception($"{Name} could not be resolved, using the given paths.");
             }
+        }
+
+        public async override void PostInitialisation()
+        {
+            if (DataGraph != null)
+                DataGraph.InitPlot("Date", "Value", format: "dd-MMM", relative: false);
         }
 
         protected async override void CheckForChange_Elapsed(object stateinfo)
@@ -83,8 +101,15 @@ namespace MopsBot.Data.Tracker
 
                 if(PastInformation == null) PastInformation = newInformation;
 
-                var embed = createEmbed(newInformation, PastInformation);
-                if(embed.Fields.Length > 0){
+                var embed = createEmbed(newInformation, PastInformation, out bool changed);
+                if(changed){
+                    var graphValue = newInformation.FirstOrDefault(x => x.Key.StartsWith("graph:"));
+
+                    if(!graphValue.Equals(default(KeyValuePair<string,string>))){
+                        DataGraph.AddValue("Value", double.Parse(PastInformation[graphValue.Key]), relative: false);
+                        DataGraph.AddValue("Value", double.Parse(graphValue.Value), relative: false);
+                    }
+
                     foreach (var channel in ChannelMessages.Keys.ToList()){
                         await OnMajorChangeTracked(channel, embed, ChannelMessages[channel]);
                     }
@@ -105,7 +130,10 @@ namespace MopsBot.Data.Tracker
             var result = new Dictionary<string, string>();
 
             foreach(string cur in ToTrack){
-                string[] keywords = cur.Split("->");
+                string curMod = cur;
+                if(cur.StartsWith("graph:")) curMod = cur.Remove(0, 6);
+                else if(cur.StartsWith("always:")) curMod = cur.Remove(0, 7);
+                string[] keywords = curMod.Split("->");
                 var tmpJson = json;
 
                 foreach(string keyword in keywords){
@@ -120,7 +148,7 @@ namespace MopsBot.Data.Tracker
             return result;
         }
 
-        private Embed createEmbed(Dictionary<string, string> newInformation, Dictionary<string, string> oldInformation)
+        private Embed createEmbed(Dictionary<string, string> newInformation, Dictionary<string, string> oldInformation, out bool changed)
         {
             var embed = new EmbedBuilder();
             embed.WithColor(255, 227, 21);
@@ -130,14 +158,21 @@ namespace MopsBot.Data.Tracker
                                    x.IconUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/JSON_vector_logo.svg/160px-JSON_vector_logo.svg.png";
                             });
 
+            changed = false;
             foreach(var kvp in newInformation){
                 string oldS = oldInformation[kvp.Key];
                 string newS = kvp.Value;
 
                 if(!newS.Equals(oldS)){
-                    embed.AddField(kvp.Key, kvp.Value.GetType().IsArray ? newS : $"{oldS} -> {newS}");
+                    changed = true;
+                    embed.AddField(kvp.Key.Split("->").Last(), $"{oldS} -> {newS}", true);
+                }
+                else if(kvp.Key.StartsWith("always:")){
+                    embed.AddField(kvp.Key.Split("->").Last(), newS, true);
                 }
             }
+
+            if(DataGraph != null) embed.ImageUrl = DataGraph.DrawPlot();
 
             return embed.Build();
         }
