675611cb843f27254d6a4977f11ef7373135e2c9
May Baer <baermay98@gmail.com>
2019-09-06 08:39:02 +0200

    Typo

--- Data/Entities/User.cs
+++ Data/Entities/User.cs
@@ -170,13 +170,13 @@ namespace MopsBot.Data.Entities
             if(MessageGraph != null){
                 InitPlot();
                 if(MessageGraph.PlotDataPoints.Last().Value.Key < OxyPlot.Axes.DateTimeAxis.ToDouble(DateTime.Today)){
-                    MessageGraph.AddValue("Value", MessageGraph.PlotDataPoints.Last().Value.Key, OxyPlot.Axes.DateTimeAxis.ToDateTime(MessageGraph.PlotDataPoints.Last().Value.Key).AddDays(1).AddMilliseconds(-1), false, false);
+                    MessageGraph.AddValue("Value", MessageGraph.PlotDataPoints.Last().Value.Value, OxyPlot.Axes.DateTimeAxis.ToDateTime(MessageGraph.PlotDataPoints.Last().Value.Key).AddDays(1).AddMilliseconds(-1), false, false);
                     MessageGraph.AddValue("Value", 0, OxyPlot.Axes.DateTimeAxis.ToDateTime(MessageGraph.PlotDataPoints.Last().Value.Key).AddDays(1), false, false);
                     MessageGraph.AddValue("Value", 0, DateTime.Now, false, false);
                 }
                 else
                     MessageGraph.AddValue("Value", MessageGraph.PlotDataPoints.Last().Value.Value, DateTime.Now, false, false);
-                    
+
                 e.WithImageUrl(MessageGraph.DrawPlot());
             }
 
