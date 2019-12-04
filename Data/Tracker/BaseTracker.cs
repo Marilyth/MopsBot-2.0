00e6c84b1f4fe6f73e7e2166c67c68b7437578c9
May Baer <baermay98@gmail.com>
2019-12-04 15:34:32 +0100

    Read page via our own method, then pass it to XmlReader

--- Data/Tracker/BaseTracker.cs
+++ Data/Tracker/BaseTracker.cs
@@ -78,9 +78,11 @@ namespace MopsBot.Data.Tracker
         {
             try
             {
+                var content = await MopsBot.Module.Information.GetURLAsync(url, headers);
+                var stream = new MemoryStream(Encoding.UTF8.GetBytes(content));
                 var settings = new System.Xml.XmlReaderSettings();
                 settings.DtdProcessing = System.Xml.DtdProcessing.Parse;
-                using (var reader = System.Xml.XmlReader.Create(url, settings))
+                using (var reader = System.Xml.XmlReader.Create(stream, settings))
                 {
                     
                     SyndicationFeed feed = SyndicationFeed.Load(reader);
