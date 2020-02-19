85302061eca5b78e1f727a6877889d054a95c8d1
May Baer <baermay98@gmail.com>
2020-02-19 16:15:36 +0100

    Add latency to status

--- StaticBase.cs
+++ StaticBase.cs
@@ -174,6 +174,10 @@ namespace MopsBot
             {
                 await Program.MopsLog(new LogMessage(LogSeverity.Error, "", "discord bot list api failed", e));
             }
+
+            await Task.Delay(30000);
+            foreach(var client in Program.Client.Shards)
+                await client.SetActivityAsync(new Game($"{client.Latency}ms Latency", ActivityType.Listening));
         }
 
         public static async Task UserVoted(IDblEntity user)
