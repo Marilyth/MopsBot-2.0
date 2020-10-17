01ef86857967c557b2e19358a83a752070221d65
Sebastian Kramm <sebastian.kramm@protonmail.com>
2020-10-17 21:55:43 +0200

    Allow build context outside of Docker folder

--- Docker/CreateDocker.sh
+++ Docker/CreateDocker.sh
@@ -2,7 +2,6 @@
 cd ..
 dotnet publish -r linux-x64 -p:PublishSingleFile=true --self-contained true
 
-cd ./Docker
-docker build --tag mopsbot:latest .
+docker build -f ./Docker/Dockerfile --tag mopsbot:latest .
 docker tag mopsbot:latest marilyth/mopsbot:latest
 docker push marilyth/mopsbot:latest
