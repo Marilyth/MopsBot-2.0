0c72f46949cd9814e52aec42cd1701e99302e2dc
May <baermay98@gmail.com>
2026-05-29 13:37:16 +0200

    Update YoutubeLiveTracker.cs (#101)

--- Data/Tracker/YoutubeLiveTracker.cs
+++ Data/Tracker/YoutubeLiveTracker.cs
@@ -106,14 +106,14 @@ namespace MopsBot.Data.Tracker
         public static async Task<string> scrapeLivestreamId(string channelId)
         {
             var html = await MopsBot.Module.Information.GetURLAsync($"https://www.youtube.com/channel/{channelId}/streams");
-            var videoSegment = html.Split("\"runs\":[{\"text\":\"LIVE\"", 2).LastOrDefault();
+            var videoSegment = html.Split("\"text\":\"LIVE\"", 2).LastOrDefault();
             if (videoSegment == null || html.Length == videoSegment.Length)
             {
                 return null;
             }
             else
             {
-                videoSegment = videoSegment.Split("VideoId\":\"", 2).LastOrDefault();
+                videoSegment = videoSegment.Split("videoId\":\"", 2).LastOrDefault();
                 var videoId = videoSegment.Split("\"").FirstOrDefault();
                 return videoId;
             }
